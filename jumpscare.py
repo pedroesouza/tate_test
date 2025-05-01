ffb9c576dea5c095f88fc40de6d7e26d3ece4f5a
PurpleDG <evan.mccabe@ucas-edu.net>
2025-05-01 14:04:20 -0600

    Entered pseudocode and made a couple if statements

---
+++ jumpscare.py
@@ -0,0 +1,23 @@
+#DEFINE jumpscare_manager function with the type parameter
+def jumpscare_manager(type):
+
+    #SET tateImage variable TO LOAD image "tate_image.png"
+    #SET deadPerson variable TO LOAD image "dead_person.png"
+    #SET tateJumpscareSound variable TO LOAD sound "jumpscare_sound.wav"
+    #SET vineBoom TO LOAD sound "vine_boom.wav"
+
+    #IF type is normal
+    if type == "normal":
+
+        pass
+
+        #scare IS class Jumpscare (tateimage, tateJumpscareSound, "MUST HAVE BEEN THE WIND", vineBoom)
+
+    #ELSE IF type is won
+    elif type == "won":
+
+        pass
+
+        #scare IS class Jumpscare (deadPerson, tateJumpscareSound, "YOU DIE :(", vineBoom)
+
+    #scare.jumpscare_run
