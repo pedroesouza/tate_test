e8f2e6e130ea18052e4612eb8caca91cf6dcee9a
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-05-05 13:41:13 -0600

    fixed stuff

--- questionFunction.py
+++ questionFunction.py
@@ -2,21 +2,7 @@ import requests
 import csv
 import os
 
-def address():
-    # Use your own IP by leaving out the IP part of the URL
-    response = requests.get('https://ipinfo.io/json')
-    
-    if response.status_code == 200:
-        data = response.json()
-        IP = data.get('ip')
-        City = data.get('city')
-        Region = data.get('region')
-        Country = data.get('country')
-        LatLong = data.get('loc')
-        WiFi = data.get('org')
-        area_code = data.get('postal')
-        time_zone = data.get('timezone')
-
+def name():
         try:
             name = os.getlogin()
         except Exception:
@@ -29,9 +15,6 @@ def address():
             formatted_name[0] = formatted_name[0].capitalize()
             formatted_name = ' '.join(formatted_name)
 
-        print(f'Well {formatted_name}, it says here that you are in {City}, {Region}, {Country}. Your current IP address is {IP} and your latitude and longitude is {LatLong}. Your WiFi network is {WiFi} and your postal/area code is {area_code}. You are also in the {time_zone} time zone.')
-    else:
-        print("Failed to retrieve location")
 
 # gets your own location
 # get_location("8.8.8.8")  # gets Google's public DNS location
@@ -52,7 +35,7 @@ def question_teller():
         for row in reader:
             question = input(f'{row[3]}: ')
             if row[0] == '5':
-                address()
+                name()
                 continue
             if row[4] == 'text entry':
                 print(row[5])
