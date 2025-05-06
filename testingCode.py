43ead1be3960d3bb068c2872cbb5dd837d47edaa
PurpleDG <evan.mccabe@ucas-edu.net>
2025-05-06 13:21:26 -0600

    Quick Fix

--- testingCode.py
+++ testingCode.py
@@ -15,7 +15,7 @@ scrn = pygame.display.set_mode((X, Y))
 pygame.display.set_caption('image')
  
 # create a surface object, image is drawn on it.
-imp = pygame.image.load("mainComputer.png").convert()
+imp = pygame.image.load("Images\mainComputer.png").convert()
  
 # Using blit to copy content from one surface to other
 scrn.blit(imp, (0, 0))
