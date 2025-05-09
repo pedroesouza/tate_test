81a48ffa21a34f9086e429754f994e9ad5e3e366
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-05-09 13:31:18 -0600

    saving in case

--- questionFunction.py
+++ questionFunction.py
@@ -1,8 +1,6 @@
 import pygame
 import csv
-import os
 import time
-import getpass
 import sys
 
 # ---------------------------- Pygame Setup ----------------------------
@@ -14,27 +12,62 @@ font = pygame.font.SysFont(None, 48)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-photo_num = "mainComputer"  # initial image
-
-# ---------------------------- Image Loading ----------------------------
-def load_image(image_name):
-    path = f"Images/{image_name}.png"
+def load_image(image_name, scale=1.0):
     try:
-        image = pygame.image.load(path).convert()
+        image = pygame.image.load(f"Images/{image_name}.png").convert()
+        if scale != 1.0:
+            w = int(image.get_width() * scale)
+            h = int(image.get_height() * scale)
+            image = pygame.transform.smoothscale(image, (w, h))
         return image
-    except:
+    except Exception as e:
+        print(f"Error loading image '{image_name}': {e}")
         return None
 
-# ---------------------------- Rendering ----------------------------
+def camera(start_image="mainComputer", scale=0.5):
+    pygame.init()
+    X, Y = WIDTH, HEIGHT
+    scrn = pygame.display.set_mode((X, Y))
+    pygame.display.set_caption('image')
+
+    def update_image(image_name):
+        img = load_image(image_name, scale)
+        if img:
+            scrn.fill((0, 0, 0))
+            x = (X - img.get_width()) // 2
+            y = (Y - img.get_height()) // 2
+            scrn.blit(img, (x, y))
+            pygame.display.flip()
+        return image_name
+
+    current_image = update_image(start_image)
+
+    status = True
+    while status:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                status = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    current_image = update_image("up1")
+                elif event.key == pygame.K_a:
+                    current_image = update_image("left1")
+                elif event.key == pygame.K_s:
+                    current_image = update_image("down1")
+                elif event.key == pygame.K_d:
+                    current_image = update_image("right1")
+                elif event.key == pygame.K_q:
+                    status = False
+
+    pygame.quit()
+    return current_image
+
 def draw_screen(question, user_input='', image_name='mainComputer'):
     screen.fill(WHITE)
-
-    # Draw background image
     image = load_image(image_name)
     if image:
         screen.blit(image, (0, 0))
 
-    # Render text
     question_surface = font.render(question, True, BLACK)
     input_surface = font.render(user_input, True, BLACK)
 
@@ -45,11 +78,11 @@ def draw_screen(question, user_input='', image_name='mainComputer'):
     screen.blit(input_surface, input_rect)
     pygame.display.flip()
 
-def show_message(message, wait_time=2):
+def show_message(message, photo_num, wait_time=2):
     draw_screen(message, '', photo_num)
     time.sleep(wait_time)
 
-def get_user_input(question_text):
+def get_user_input(question_text, photo_num):
     user_input = ''
     while True:
         draw_screen(question_text, user_input, photo_num)
@@ -64,14 +97,13 @@ def get_user_input(question_text):
                     user_input = user_input[:-1]
                 else:
                     user_input += event.unicode
-                    
+
 # ---------------------------- Survey Logic ----------------------------
-def question_teller():
-    global photo_num
+def question_teller(photo_num):
     score = 0
     total_score = 0
 
-    show_message("Welcome to the survey.")
+    show_message("Welcome to the survey.", photo_num)
     time.sleep(1)
 
     with open('questions.csv', 'r') as file:
@@ -80,154 +112,157 @@ def question_teller():
         for row in reader:
             id = row[0]
             question_text = f"Question {id}: {row[1]}"
-            question = get_user_input(question_text)
+            question = get_user_input(question_text, photo_num)
 
             if id == '9':
-                show_message("Let's play a little game, shall we?")
+                show_message("Let's play a little game, shall we?", photo_num)
             if id == '13' and question == '10':
                 break
             if id == '1' and question == 'yes':
-                show_message("You can't play this game.")
+                show_message("You can't play this game.", photo_num)
                 break
 
             if id == '1':
                 while True:
                     if question == 'yes':
-                        show_message("You can't play this game then. Bye.")
+                        show_message("You can't play this game then. Bye.", photo_num)
                         return
                     elif question == 'no':
-                        show_message("Ok good.")
+                        show_message("Ok good.", photo_num)
                         break
                     else:
-                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)")
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
 
             elif id == '2':
-                show_message("Ok.")
+                show_message("Ok.", photo_num)
             elif id == '3':
-                show_message("That's cool.")
+                show_message("That's cool.", photo_num)
             elif id == '4':
                 while True:
                     if question == 'yes':
-                        show_message("Ok, just double checking.")
+                        show_message("Ok, just double checking.", photo_num)
                         break
                     elif question == 'no':
-                        show_message("Are you sure?...")
+                        show_message("Are you sure?...", photo_num)
                         break
                     else:
-                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)")
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
             elif id == '5':
-                photo_num = 'up1'  # Update background image
-                show_message("Actually based off my IP grabber, you are at 31.255.56.229")
+                photo_num = 'up1'
+                show_message("Actually based off my IP grabber, you are at 31.255.56.229", photo_num)
             elif id == '6':
                 while True:
                     if question == 'yes':
                         photo_num = 'right1'
-                        show_message("Ok cool, our team is ready to move in...")
+                        show_message("Ok cool, our team is ready to move in...", photo_num)
                         break
                     elif question == 'no':
                         photo_num = 'left1'
-                        show_message("Not anymore...")
+                        show_message("Not anymore...", photo_num)
                         break
                     else:
-                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)")
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
             elif id == '7':
                 while True:
                     try:
                         int(question)
-                        show_message("Thanks.")
+                        show_message("Thanks.", photo_num)
                         break
                     except ValueError:
-                        question = get_user_input(f"Question {id}: {row[1]} (type a number)")
+                        question = get_user_input(f"Question {id}: {row[1]} (type a number)", photo_num)
             elif id == '8':
                 while True:
                     if question == 'yes':
-                        show_message("Good boy.")
+                        show_message("Good boy.", photo_num)
                         break
                     elif question == 'no':
-                        show_message("Bad boy...")
+                        show_message("Bad boy...", photo_num)
                         break
                     else:
-                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)")
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
             elif id == '9a':
                 if question == '19':
-                    show_message("Correct!")
+                    show_message("Correct!", photo_num)
                     score += 1
                 else:
-                    show_message("Incorrect...")
+                    show_message("Incorrect...", photo_num)
                 total_score += 1
             elif id == '9b':
                 if question == '2':
-                    show_message("Correct!")
+                    show_message("Correct!", photo_num)
                     score += 1
                 else:
-                    show_message("Incorrect...")
+                    show_message("Incorrect...", photo_num)
                 total_score += 1
             elif id == '9c':
                 if question == '110':
-                    show_message("Correct!")
+                    show_message("Correct!", photo_num)
                     score += 1
                 else:
-                    show_message("Incorrect...")
+                    show_message("Incorrect...", photo_num)
                 total_score += 1
             elif id == '9d':
                 if question == '63':
-                    show_message("Correct!")
+                    show_message("Correct!", photo_num)
                     score += 1
                 else:
-                    show_message("Incorrect...")
+                    show_message("Incorrect...", photo_num)
                 total_score += 1
             elif id == '9e':
                 if question == '5':
-                    show_message("Correct!")
+                    show_message("Correct!", photo_num)
                     score += 1
                 else:
-                    show_message("Incorrect...")
+                    show_message("Incorrect...", photo_num)
                 total_score += 1
             elif total_score == 5:
                 if score >= 3:
-                    show_message("You failed...")
+                    show_message("You failed...", photo_num)
                 else:
-                    show_message("You passed.")
+                    show_message("You passed.", photo_num)
             elif id == '10':
                 photo_num = 'down1'
                 while True:
                     if question == 'yes':
-                        show_message("Nuh uh.")
+                        show_message("Nuh uh.", photo_num)
                         break
                     elif question == 'no':
-                        show_message("Good.")
+                        show_message("Good.", photo_num)
                         break
                     else:
-                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)")
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
             elif id == '11':
                 while True:
                     if question in ['yes', 'no']:
-                        show_message("Ok.")
+                        show_message("Ok.", photo_num)
                         break
                     else:
-                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)")
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
             elif id == '12':
                 while True:
                     if question == 'yes':
-                        show_message("How did you know?")
+                        show_message("How did you know?", photo_num)
                         time.sleep(2)
                         break
                     elif question == 'no':
-                        show_message("Think again...")
+                        show_message("Think again...", photo_num)
                         time.sleep(2)
                         break
                     else:
-                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)")
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
             elif id == '13':
                 while True:
                     if question in [str(i) for i in range(1, 10)]:
-                        show_message("Give me a ten.")
+                        show_message("Give me a ten.", photo_num)
                     elif question == '10':
-                        show_message("Good boy.")
+                        show_message("Good boy.", photo_num)
                         break
                     else:
-                        question = get_user_input(f"Question {id}: {row[1]} (1–10)")
+                        question = get_user_input(f"Question {id}: {row[1]} (1–10)", photo_num)
+
+    return photo_num
 
 # ---------------------------- Run ----------------------------
-question_teller()
+photo_num = camera()  # returns the last viewed image name
+question_teller(photo_num)
 pygame.quit()
