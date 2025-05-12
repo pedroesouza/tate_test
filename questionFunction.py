850e1ee282fe69433b34ceda7b3600a26ec731c7
locklinsheldon <locklin.sheldon@ucas-edu.net>
2025-05-12 13:38:15 -0600

    pluh

--- questionFunction.py
+++ questionFunction.py
@@ -1,99 +1,271 @@
+import pygame
 import csv
-import os
+import time
+import sys
 
-def name():
-        try:
-            name = os.getlogin()
-        except Exception:
-            # fallback for environments where os.getlogin() might not work (e.g. some IDEs)
-            import getpass
-            name = getpass.getuser()
+# ---------------------------- Pygame Setup ----------------------------
+pygame.init()
+WIDTH, HEIGHT = 1650, 1275
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame Survey")
+font = pygame.font.SysFont(None, 48)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-        formatted_name = name.replace('.', ' ').split()
-        if formatted_name:
-            formatted_name[0] = formatted_name[0].capitalize()
-            formatted_name = ' '.join(formatted_name)
+def load_image(image_name, scale=1.0):
+    try:
+        image = pygame.image.load(f"Images/{image_name}.png").convert()
+        if scale != 1.0:
+            w = int(image.get_width() * scale)
+            h = int(image.get_height() * scale)
+            image = pygame.transform.smoothscale(image, (w, h))
+        return image
+    except Exception as e:
+        print(f"Error loading image '{image_name}': {e}")
+        return None
 
+def camera(start_image="mainComputer", scale=0.5):
+    pygame.init()
+    X, Y = WIDTH, HEIGHT
+    scrn = pygame.display.set_mode((X, Y))
+    pygame.display.set_caption('image')
 
-# gets your own location
-# get_location("8.8.8.8")  # gets Google's public DNS location
+    def update_image(image_name):
+        img = load_image(image_name, scale)
+        if img:
+            scrn.fill((0, 0, 0))
+            x = (X - img.get_width()) // 2
+            y = (Y - img.get_height()) // 2
+            scrn.blit(img, (x, y))
+            pygame.display.flip()
+        return image_name
 
-# Get the current logged-in user
-name = os.getlogin()
+    current_image = update_image(start_image)
 
-formatted_name = name.replace('.', ' ').split()
-formatted_name[0] = formatted_name[0].capitalize()
+    status = True
+    while status:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                status = False
+            elif event.type == pygame.KEYDOWN:  # Detect key press
+                if event.key == pygame.K_UP:  # Up arrow key
+                    update_image("Images/up1.png")
+                elif event.key == pygame.K_LEFT:  # Left arrow key
+                    update_image("Images/left1.png")
+                elif event.key == pygame.K_DOWN:  # Down arrow key
+                    update_image("Images/down1.png")
+                elif event.key == pygame.K_RIGHT:  # Right arrow key
+                    update_image("Images/right1.png")
+                elif event.key == pygame.K_q:  # Q key to quit
+                    status = False
+            elif event.type == pygame.KEYUP:  # Detect key release
+                # Reset to the mainComputer image when no key is pressed
+                update_image("Images/mainComputer.png")
 
-# Join the parts back together
-formatted_name = ' '.join(formatted_name)
+    pygame.quit()
+    return current_image
 
-def question_teller():
+def draw_screen(question, user_input='', image_name='mainComputer'):
+    screen.fill(WHITE)
+    image = load_image(image_name)
+    if image:
+        screen.blit(image, (0, 0))
+
+    question_surface = font.render(question, True, BLACK)
+    input_surface = font.render(user_input, True, BLACK)
+
+    question_rect = question_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 40))
+    input_rect = input_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 10))
+
+    screen.blit(question_surface, question_rect)
+    screen.blit(input_surface, input_rect)
+    pygame.display.flip()
+
+def show_message(message, photo_num, wait_time=2):
+    draw_screen(message, '', photo_num)
+    time.sleep(wait_time)
+
+def get_user_input(question_text, photo_num):
+    user_input = ''
+    while True:
+        draw_screen(question_text, user_input, photo_num)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return user_input.strip().lower()
+                elif event.key == pygame.K_BACKSPACE:
+                    user_input = user_input[:-1]
+                else:
+                    user_input += event.unicode
+
+# ---------------------------- Survey Logic ----------------------------
+def question_teller(photo_num):
+    score = 0
+    total_score = 0
+
+    show_message("Welcome to the survey.", photo_num)
+    time.sleep(1)
+
     with open('questions.csv', 'r') as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
             id = row[0]
-            score = 0
+            question_text = f"Question {id}: {row[1]}"
+            question = get_user_input(question_text, photo_num)
+
             if id == '9':
-                print("Let's play a little game, shall we?")
-            while True:
-                question = input(f'Question {id}: {row[1]}: ').lower().strip()
-                if id == '1':
-                    if question == 'yes': #Add this at the end for the for loop.
-                        print("You can't play this game then. Bye.")
-                        break
-                    elif question == 'no':
-                        print('Ok good.')
-                        break
-                    else:
-                        print('Incorrect option, try again. (type yes or no).')
-                if id == '2':
-                    print('Ok.')
-                    break
-                if id == '3':
-                    print("That's cool.")
-                    break
-                if id == '4':
+                show_message("Let's play a little game, shall we?", photo_num)
+            if id == '13' and question == '10':
+                break
+            if id == '1' and question == 'yes':
+                show_message("You can't play this game.", photo_num)
+                break
+
+            if id == '1':
+                while True:
                     if question == 'yes':
-                        print('Ok, just double checking.')
-                        break
+                        show_message("You can't play this game then. Bye.", photo_num)
+                        return
                     elif question == 'no':
-                        print('Are you sure?...')
+                        show_message("Ok good.", photo_num)
                         break
                     else:
-                        print('Incorrect option, try again. (type yes or no).')
-                if id == '5':
-                    print('Actually based off my IP grabber, you are at 31.255.56.229')
-                    break
-                if id == '6':
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
+
+            elif id == '2':
+                show_message("Ok.", photo_num)
+            elif id == '3':
+                show_message("That's cool.", photo_num)
+            elif id == '4':
+                while True:
                     if question == 'yes':
-                        print('Ok cool, our team is ready to move in...')
+                        show_message("Ok, just double checking.", photo_num)
                         break
                     elif question == 'no':
-                        print('Not anymore...')
+                        show_message("Are you sure?...", photo_num)
                         break
                     else:
-                        print('Incorrect option, try again. (type yes or no).')
-                if id == '7':
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
+            elif id == '5':
+                photo_num = 'up1'
+                show_message("Actually based off my IP grabber, you are at 31.255.56.229", photo_num)
+            elif id == '6':
+                while True:
+                    if question == 'yes':
+                        photo_num = 'right1'
+                        show_message("Ok cool, our team is ready to move in...", photo_num)
+                        break
+                    elif question == 'no':
+                        photo_num = 'left1'
+                        show_message("Not anymore...", photo_num)
+                        break
+                    else:
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
+            elif id == '7':
+                while True:
                     try:
                         int(question)
-                        print('Thanks.')
+                        show_message("Thanks.", photo_num)
                         break
                     except ValueError:
-                        print('Incorrect option, try again. (type a number).')
-                if id == '8':
+                        question = get_user_input(f"Question {id}: {row[1]} (type a number)", photo_num)
+            elif id == '8':
+                while True:
                     if question == 'yes':
-                        print('Good boy.')
+                        show_message("Good boy.", photo_num)
                         break
                     elif question == 'no':
-                        print('Bad boy...')
-                        #jumpscare
+                        show_message("Bad boy...", photo_num)
                         break
-                if id == '9a':
-                    if question == '19':
-                        print('Correct!')
-                        score += 1
-                    
-                
+                    else:
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
+            elif id == '9a':
+                if question == '19':
+                    show_message("Correct!", photo_num)
+                    score += 1
+                else:
+                    show_message("Incorrect...", photo_num)
+                total_score += 1
+            elif id == '9b':
+                if question == '2':
+                    show_message("Correct!", photo_num)
+                    score += 1
+                else:
+                    show_message("Incorrect...", photo_num)
+                total_score += 1
+            elif id == '9c':
+                if question == '110':
+                    show_message("Correct!", photo_num)
+                    score += 1
+                else:
+                    show_message("Incorrect...", photo_num)
+                total_score += 1
+            elif id == '9d':
+                if question == '63':
+                    show_message("Correct!", photo_num)
+                    score += 1
+                else:
+                    show_message("Incorrect...", photo_num)
+                total_score += 1
+            elif id == '9e':
+                if question == '5':
+                    show_message("Correct!", photo_num)
+                    score += 1
+                else:
+                    show_message("Incorrect...", photo_num)
+                total_score += 1
+            elif total_score == 5:
+                if score >= 3:
+                    show_message("You failed...", photo_num)
+                else:
+                    show_message("You passed.", photo_num)
+            elif id == '10':
+                photo_num = 'down1'
+                while True:
+                    if question == 'yes':
+                        show_message("Nuh uh.", photo_num)
+                        break
+                    elif question == 'no':
+                        show_message("Good.", photo_num)
+                        break
+                    else:
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
+            elif id == '11':
+                while True:
+                    if question in ['yes', 'no']:
+                        show_message("Ok.", photo_num)
+                        break
+                    else:
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
+            elif id == '12':
+                while True:
+                    if question == 'yes':
+                        show_message("How did you know?", photo_num)
+                        time.sleep(2)
+                        break
+                    elif question == 'no':
+                        show_message("Think again...", photo_num)
+                        time.sleep(2)
+                        break
+                    else:
+                        question = get_user_input(f"Question {id}: {row[1]} (yes/no)", photo_num)
+            elif id == '13':
+                while True:
+                    if question in [str(i) for i in range(1, 10)]:
+                        show_message("Give me a ten.", photo_num)
+                    elif question == '10':
+                        show_message("Good boy.", photo_num)
+                        break
+                    else:
+                        question = get_user_input(f"Question {id}: {row[1]} (1–10)", photo_num)
 
-question_teller()
+    return photo_num
+
+# ---------------------------- Run ----------------------------
+photo_num = camera()  # returns the last viewed image name
+question_teller(photo_num)
+pygame.quit()
---
+++ test.py
@@ -0,0 +1,50 @@
+# importing required library
+import pygame
+
+# activate the pygame library
+pygame.init()
+X = 1650
+Y = 1275
+
+# create the display surface object
+scrn = pygame.display.set_mode((X, Y))
+
+# set the pygame window name
+pygame.display.set_caption('image')
+
+# load initial image
+current_image = pygame.image.load("Images/mainComputer.png").convert()
+
+# function to update the screen with a new image
+def update_image(image_path):
+    global current_image
+    current_image = pygame.image.load(image_path).convert()
+    scrn.blit(current_image, (0, 0))
+    pygame.display.flip()
+
+# draw the initial image
+scrn.blit(current_image, (0, 0))
+pygame.display.flip()
+
+status = True
+while status:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status = False
+        elif event.type == pygame.KEYDOWN:  # Detect key press
+            if event.key == pygame.K_UP:  # Up arrow key
+                update_image("Images/up1.png")
+            elif event.key == pygame.K_LEFT:  # Left arrow key
+                update_image("Images/left1.png")
+            elif event.key == pygame.K_DOWN:  # Down arrow key
+                update_image("Images/down1.png")
+            elif event.key == pygame.K_RIGHT:  # Right arrow key
+                update_image("Images/right1.png")
+            elif event.key == pygame.K_q:  # Q key to quit
+                status = False
+        elif event.type == pygame.KEYUP:  # Detect key release
+            # Reset to the mainComputer image when no key is pressed
+            update_image("Images/mainComputer.png")
+
+# deactivates the pygame library
+pygame.quit()
