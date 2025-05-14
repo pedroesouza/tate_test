e03207b312aaa59210b705a8f3c95df3c6d1c71c
PurpleDG <evan.mccabe@ucas-edu.net>
2025-05-14 13:11:40 -0600

    stuff

--- exitFunction.py
+++ exitFunction.py
@@ -1,33 +1,54 @@
 import cv2
 import pygame
 from pygame import mixer 
-  
-# Starting the mixer 
-mixer.init() 
-  
-# Loading the song 
-mixer.music.load("Bye Bye Bye Audio.mp3") 
-  
-# Setting the volume 
-mixer.music.set_volume(0.7) 
-  
-# Start playing the song 
-mixer.music.play() 
+import time
+
+def exitFunction():
+
+    go = input("\nWould you like to exit the program? (y/n)\n")
+
+    if go != "y":
+        
+        print("\nTry again.\n")
+
+        time.sleep(1)
+
+        exitFunction()
+
+    else:
+
+        print("\nGoodbye!")
+
+        time.sleep(1)
+
+        # Starting the mixer 
+        mixer.init() 
+        
+        # Loading the song 
+        mixer.music.load("Bye Bye Bye Audio.mp3") 
+        
+        # Setting the volume 
+        mixer.music.set_volume(0.7) 
+        
+        # Start playing the song 
+        mixer.music.play() 
 
 
-cap = cv2.VideoCapture("Bye Bye Bye x2 Speed.mp4")
-success, img = cap.read()
-shape = img.shape[1::-1]
-wn = pygame.display.set_mode(shape)
-clock = pygame.time.Clock()
+        cap = cv2.VideoCapture("Bye Bye Bye x2 Speed.mp4")
+        success, img = cap.read()
+        shape = img.shape[1::-1]
+        wn = pygame.display.set_mode(shape)
+        clock = pygame.time.Clock()
 
-while success:
-    clock.tick(60)
-    success, img = cap.read()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            success = False
-    wn.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
-    pygame.display.update()
+        while success:
+            clock.tick(60)
+            success, img = cap.read()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    success = False
+            wn.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
+            pygame.display.update()
 
-pygame.quit()
+        pygame.quit()
+    
+exitFunction()
